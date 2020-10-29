c484a02acffc647f96063d9d1dc43654ebc47516
kasirajan22 <kasi.srm@gmail.com>
2020-10-30 05:11:14 +0530

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Magizhan on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
